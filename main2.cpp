2598d875850bdb9e71e2839391110e267b164175
hiddevuijk <hdvuijk@gmail.com>
2016-06-16 16:25:06 +0200

    finished multidim. adapt rkint. Check error calculation.

--- main2.cpp
+++ main2.cpp
@@ -5,10 +5,10 @@ main program for testing the Runge-Kutta integration scheme
 */
 
 
-#include "integrator.h"
-#include "eulerInt.h"
-#include "rkInt.h"
-#include "adaptRKInt.h"
+//#include "integrator.h"
+//#include "eulerInt.h"
+//#include "rkInt.h"
+#include "adaptRK.h"
 
 #include <iostream>
 #include <string>
@@ -18,23 +18,33 @@ main program for testing the Runge-Kutta integration scheme
 
 using namespace::std;
 
-void der(const double&x , const double& y, double& dydx)
+void der(const double&x , const vector<double>& y, vector<double>& dydx)
 {
-	dydx = -0.1*y + y/x;
+	for(int i=0;i<y.size();++i)
+		dydx[i] = -0.1*y[i] + y[i]/x ;
+
+}
+void der0(const double&x , const double& y, double& dydx)
+{
+
+	dydx = -0.1*y+ y/x;
 
 }
 
+
 int main()
 {
-
-	double x = 1;
+	double x = 1.;
 	double xend = 20.;
-	double y = 1.;
+	vector<double> y(2,1.);
 	adaptIntegrator(x,y,xend,adaptRKInt,der);
 
-
-	cout << y << endl << xend << endl;
-
+	x = 1.;
+	double yy = 1.;
+	adaptIntegrator(x,yy,xend,adaptRKInt,der0);
+	cout << y[0] << '\t' << y[1]  << endl;
+	cout << yy << endl;
+	cout << endl << y[0] - yy << endl;
 	return 0;
 }
 
